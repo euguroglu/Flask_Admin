c5b46639531f2ebe3d2ca8ad11cc0cf599977c21
euguroglu <e.uguroglu@yahoo.com>
2020-12-27 17:18:20 +0300

    some configuration added

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_admin import Admin
 from flask_admin.contrib.sqla import ModelView
 import os
+from flask_admin.contrib.fileadmin import FileAdmin
+
 
 app = Flask(__name__)
 
@@ -33,8 +35,33 @@ class Comment(db.Model):
     def __repr__(self):
         return '<Comment %r>' % (self.id)
 
-admin.add_view(ModelView(User,db.session))
-admin.add_view(ModelView(Comment,db.session))
+#If i want to adjust admin view
+class UserView(ModelView):
+    #if i want to delete one column from view
+    column_exclude_list = ['password']
+    #if i want to display primary key
+    column_display_pk = True
+    #if i want to export data
+    can_export = True
+    #if i want to be able to modify or create new comment using admin view
+    inline_models = [Comment]
+    #if we set below function false user view wont be there we can set some functionality to authenticate someone to see that panel
+    def is_accessible(self):
+        return True
+    #This is comment when someone try to see admin/user page without authenticated.
+    def inaccessible_callback(self,name,**kwargs):
+        return '<h1>You are not logged in</h1>'
+
+class CommentView(ModelView):
+    #To change create model page as pop-up window
+    create_modal = True
+
+admin.add_view(UserView(User,db.session))
+admin.add_view(CommentView(Comment,db.session))
+
+#Flask admin file view
+path = os.path.join(os.path.dirname(__file__),'uploads')
+admin.add_view(FileAdmin(path,'/uploads/',name='Uploads'))
 
 if __name__ == "__main__":
     app.run(debug=True)
