90acbc0b882a72c6422b56c08fbdd34ed54dabfd
euguroglu <e.uguroglu@yahoo.com>
2020-12-27 16:41:41 +0300

    relational database in admin

--- app.py
+++ app.py
@@ -17,11 +17,24 @@ admin = Admin(app,template_mode='bootstrap3')
 class User(db.Model):
     id = db.Column(db.Integer,primary_key=True)
     username = db.Column(db.String(50))
+    password = db.Column(db.String(50))
+    age = db.Column(db.Integer)
+    birthday = db.Column(db.DateTime)
+    comments = db.relationship('Comment',backref='user',lazy='dynamic')
 
     def __repr__(self):
-        return '<User %r>' % (self.username)
+        return 'User: {}'.format(self.username)
+
+class Comment(db.Model):
+    id = db.Column(db.Integer,primary_key=True)
+    comment_text = db.Column(db.String(200))
+    user_id = db.Column(db.Integer,db.ForeignKey('user.id'))
+
+    def __repr__(self):
+        return '<Comment %r>' % (self.id)
 
 admin.add_view(ModelView(User,db.session))
+admin.add_view(ModelView(Comment,db.session))
 
 if __name__ == "__main__":
     app.run(debug=True)
